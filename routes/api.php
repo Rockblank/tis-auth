a76cd6b0eafa352ed05bddbae62dcb20b6bb0709
Archie <57532087+Rockblank@users.noreply.github.com>
2026-04-16 02:01:01 +0700

    Langkah 7

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/home', [HomeController::class, 'home'])->middleware('auth.token');
 });
